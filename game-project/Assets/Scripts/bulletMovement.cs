860f5b76bce7648018e9c157e65224c388c7200c
Nhan Phan <nhanphan24@gmail.com>
2018-10-30 23:08:22 -0700

    Transferred over

---
+++ game-project/Assets/Scripts/bulletMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletMovement : MonoBehaviour {
+
+    public float bulletSpeed = 15f;
+    public Rigidbody2D bullet;
+
+	// Use this for initialization
+	void Start () {
+        //This function tells the bullet to move depends
+        //on the speed given by variable bulletSpeed from
+        //the moment the bullet is created
+        bullet.velocity = transform.right * bulletSpeed;
+	}
+
+}
---
+++ game-project/Assets/Scripts/shooting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour {
+
+    public Transform shootingPoint;
+    public GameObject bullet;
+	// Update is called once per frame
+	void Update () {
+        // If "Fire2" is pressed down, then call the function Shoot
+        if (Input.GetButtonDown("Fire2")) 
+        {                                 
+            Shoot();                      
+        }
+	}
+    
+    //This function creates a bullet object
+    void Shoot()
+    {
+        Instantiate(bullet, shootingPoint.position, shootingPoint.rotation);
+    }
+}
